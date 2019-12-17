9ae613a8930c158fd691a05b370019b1048a3ade
Mickey Smith <smithmick14@gmail.com>
2019-12-17 15:03:48 -0600

    Textbox and buttons are laid out nicely

--- client/src/App.js
+++ client/src/App.js
@@ -23,9 +23,32 @@ class App extends React.Component {
     return (
       <div id="mainContainer" className="container-fluid">
         <div className="row">
-          <div id="leftContainer" className="col">
+          <div id="leftContainer" className="col-8">
           </div>
-          <div id="rightContainer" className="col">
+          <div id="rightContainer" className="col-4">
+            <div id="textboxRow" className="row my-4 align-middle">
+              <div id="textboxCol" className="col d-flex justify-content-around">
+                <textarea
+                  id="textbox"
+                  rows="20"
+                  placeholder="Your coordinates will show up here..."
+                  readOnly>
+
+                </textarea>
+              </div>
+            </div>
+            <div id="buttonsRow" className="row my-4 align-middle">
+              <div id="resetButtonCol" className="col d-flex justify-content-around">
+                <button id="resetButton" className="btn btn-primary">
+                  Reset
+                </button>
+              </div>
+              <div id="submitButtonCol" className="col d-flex justify-content-around">
+                <button id="submitButton" className="btn btn-success">
+                  Success
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
